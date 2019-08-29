8efa6536dde73cd3fb56885f450a373d0d28d4ae
jacob <khh119@gmail.com>
2019-08-29 22:11:15 +0900

    Initial commit

---
+++ src/main/scala/org/jacob/wordmr/App.scala
@@ -0,0 +1,21 @@
+package org.jacob.wordmr
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+}
+
+
+
+
+
+
+
